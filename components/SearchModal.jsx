43cda0e5618c674bf46a422274f72e1ee2eb2973
genialtechie <thehaleembello@gmail.com>
2022-06-28 23:11:30 -0400

    add search feature

--- components/SearchModal.jsx
+++ components/SearchModal.jsx
@@ -5,6 +5,18 @@ import FuzzySearch from 'fuzzy-search'
 
 const SearchModal = ({show, hide}) => {
   const [posts, setPosts] = useState([])
+  const [input, setInput] = useState()
+  const [results, setResults] = useState()
+
+  function handleChange(event) {
+    const query = event.target.value
+    setInput(query)
+    const searcher = new FuzzySearch(posts, ['node.title'], {
+      sort: true,
+    });
+    const res = searcher.search(input);
+    setResults(res)
+  }
 
   useEffect(async () => {
     const data = await getPosts()
@@ -18,29 +30,30 @@ const SearchModal = ({show, hide}) => {
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
         >
-          <div className='bg-gray-900'>
+          <div className='bg-gray-900' onChange={handleChange} >
             <Modal.Header closeButton>
               <form className='w-full m-2'>   
-                  <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
                   <div className="relative">
-                      <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                          <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                      </div>
-                      <input type="search" id="default-search" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                      <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                      <input type="search" id="default-search" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search articles..." required />
+                      
                   </div>
               </form>
             </Modal.Header>
             <Modal.Body>
-            <p>
-                Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                ipsam atque a dolores quisquam quisquam adipisci possimus
-                laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                deleniti rem!
-            </p>
+              <div className='m-6 flex flex-col w-full' >
+                {results.map(post => (
+                  <div>
+                    {input !== '' && <div className='pb-4 pt-2 px-4 flex flex-row' key={post.node.slug} >
+                      <span className='my-auto' ><img src={post.node.featuredImage.url} alt="" className='h-24 w-28 object-cover p-2'/></span>
+                      <span className='px-3 mt-3 text-left w-full'>
+                        <h2 className='text-xl font-bold'>{post.node.title}</h2>
+                        <span className='text-sm' >{post.node.excerpt.split(' ').slice(0, 13).join(' ').concat('...')}</span>
+                      </span>
+                    </div>}
+                  </div>
+                  
+                ))}
+              </div>
             </Modal.Body>
           </div>
         </Modal>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const index = ({posts}) => {
           <title>Loudr</title>
           <link rel="shortcut icon" href="logo.png" type="image/x-icon" />
         </Head>
-        <div className="container px-16 lg:px-32 mx-auto">
+        <div className="container px-6 lg:px-32 mx-auto">
           <PostWidget category={{"categories": "Fashion"}} />
           {posts.map((post) => <PostCard post={post.node} key={post.node.slug} />)}
           <PostWidget category={{"categories": "Tech"}} />
